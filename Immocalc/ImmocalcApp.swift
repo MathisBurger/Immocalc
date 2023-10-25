4bb3685f799cce09adadbe5f466d099e805c914a
Mathis Burger <mburger@die-netzwerkstatt.de>
2023-10-25 15:27:22 +0200

    Initial Commit

---
+++ Immocalc/ImmocalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmocalcApp.swift
+//  Immocalc
+//
+//  Created by Mathis Burger on 25.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ImmocalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
